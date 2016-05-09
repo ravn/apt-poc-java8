f74c6847e5e290231e5d127b1ee5319d9b5aea5a
Thorbjoern Ravn Andersen <tra@statsbiblioteket.dk>
2016-05-09 15:11:13 +0200

    added "poc-tool" deployment skeleton.

---
+++ tools/poc-tool/src/main/java/dk/statsbiblioteket/user/tra/apt_poc/poc_tool/Main.java
@@ -0,0 +1,12 @@
+package dk.statsbiblioteket.user.tra.apt_poc.poc_tool;
+
+import java.util.Arrays;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world - args = " + Arrays.asList(args));
+    }
+}
